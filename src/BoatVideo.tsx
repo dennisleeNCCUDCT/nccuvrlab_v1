ca168c099b782080be61402c22ffe99cf809f180
dennisleeNCCUDCT <95065965+dennisleeNCCUDCT@users.noreply.github.com>
2023-07-13 10:47:43 +0800

    plzzzz work!!!!!!

--- src/BoatVideo.tsx
+++ src/BoatVideo.tsx
@@ -1,3 +1,5 @@
+import video from "./assets/UE5 Ship Sailing_1080p.mp4";
+
 function BoatVideo() {
   return (
     <video
@@ -5,12 +7,8 @@ function BoatVideo() {
       autoPlay
       muted
       loop
-    >
-      <source
-        src="https://www.youtube.com/watch?v=ijF4-Dea4bM&ab_channel=EranHaviv"
-        type="video/mp4"
-      />
-    </video>
+      src={video}
+    ></video>
   );
 }
 
